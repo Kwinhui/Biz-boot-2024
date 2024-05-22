332dfa0ddb188aa947c4fc6e09b7d09eda3244ad
n96js <n96js@naver.com>
2024-05-22 11:42:34 +0900

    hello

---
+++ Sp-boot-hello-2/src/main/java/com/callor/hello/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.callor.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping(value = {"/"})
+    public String home() {
+        
+        return "안녕하세요";
+    }
+    
+}
