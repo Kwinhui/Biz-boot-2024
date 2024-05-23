09acc3d29580e28d84cf903b088dd3a7cef940f4
n96js <n96js@naver.com>
2024-05-23 09:30:02 +0900

    Book_Project_시작

---
+++ Sp-boot-book-1/src/test/java/com/collor/book/BookProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collor.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
